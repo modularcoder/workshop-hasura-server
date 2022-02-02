72c3e259f81f89ad7b8a7de5f94d48591cbcaa80
Gevorg Harutyunyan <gevharut@gmail.com>
2022-02-02 19:26:46 +0400

    Seed data
    Signed-off-by: Gevorg Harutyunyan <gevharut@gmail.com>

---
+++ hasura/seeds/default/1643815588199_init.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public.app_user (id, created_at, updated_at, username) VALUES ('49b34462-8d4f-4531-bd02-b08ede3c0982', '2022-02-02 15:25:02.309795+00', '2022-02-02 15:25:02.309795+00', 'dude');
+INSERT INTO public.tweet (id, created_at, updated_at, user_id, content) VALUES ('56ea4701-ebd6-407e-a8a8-662036b30c61', '2022-02-02 15:25:16.598408+00', '2022-02-02 15:25:16.598408+00', '49b34462-8d4f-4531-bd02-b08ede3c0982', 'Hello World');
